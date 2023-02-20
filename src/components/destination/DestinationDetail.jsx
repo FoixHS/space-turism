d73a3b04ffdb0c85db271941cf0a4ed96e07b474
Hernan Foix <hernan_foix@hotmail.com>
2023-02-19 23:37:33 -0300

    last destination styles

--- src/components/destination/DestinationDetail.jsx
+++ src/components/destination/DestinationDetail.jsx
@@ -41,7 +41,7 @@ export const DestinationDetail = () => {
   const [optionSelected, setOptionSelected] = useState(options[0].id)
 
   return (
-    <div className='p-8 md:max-w-[595px] lg:max-w-full mx-auto lg:mx-0 flex flex-col lg:flex-row lg:items-end space-y-6 md:space-y-14 lg:space-y-0'>
+    <div className='p-8 md:max-w-[595px] lg:max-w-full mx-auto lg:mx-0 flex flex-col lg:flex-row lg:items-end space-y-6 md:space-y-14 lg:space-y-0 lg:space-x-5'>
       <div className='w-[170px] md:w-[300px] lg:w-[445px] xl:w-[500px] mx-auto'>
         <img className='w-full object-cover' src={options[optionSelected].imageUrl} alt={options[optionSelected].imageUrl} />
       </div>
@@ -54,7 +54,7 @@ export const DestinationDetail = () => {
         <div className='divide-y divide-solid divide-[#383B4B]'>
           <div className='pb-8 md:pb-12'>
             <h2 className='text-center lg:text-left text-6xl md:text-8xl lg:text-[100px] font-bellefair text-white uppercase'>{options[optionSelected].label}</h2>
-            <p className='mt-3 text-center lg:text-left text-[#D0D6F9] text-sm md:text-lg lg:text-xl leading-6 md:leading-8 lg:leading-10 font-barlow tracking-wide'>{options[optionSelected].description}</p>
+            <p className='max-w-[327px] md:max-w-full lg:pr-5 mt-3 text-center lg:text-left text-[#D0D6F9] text-sm md:text-lg lg:text-xl leading-6 md:leading-8 lg:leading-10 font-barlow tracking-wide'>{options[optionSelected].description}</p>
           </div>
           <div className='pt-8 md:pt-12 flex flex-col md:flex-row md:justify-evenly lg:justify-start md:items-center space-y-8 md:space-y-0 lg:space-x-20'>
             <DestinationMetadata title='Avg. distance' stats={options[optionSelected].distance} />
